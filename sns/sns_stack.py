9784d13e3cb9162df65278559cecb178c3755e3f
He <heyannan@amazon.com>
2023-01-28 16:33:16 +0800

    Add VPC endpoint for private API Gateway access in another account.

--- sns/sns_stack.py
+++ sns/sns_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
     Stack,
     aws_sns as sns,
-    aws_iam as iam
+    aws_iam as iam,
+    aws_ec2 as ec2
 )
 from constructs import Construct
 
@@ -18,8 +19,27 @@ class SnsStack(Stack):
 
         topic.add_to_resource_policy(
             iam.PolicyStatement(
-                principals=[iam.AccountPrincipal("425039140189")],
+                principals=[iam.AccountPrincipal("116266104059")],
                 actions=["SNS:Subscribe"],
                 resources=["*"]
             )
+        )
+
+        vpc = ec2.Vpc(self, "VpcForCrossAccessAPIGW")
+
+        security_group = ec2.SecurityGroup(self, "SG",
+                                           vpc=vpc,
+                                           security_group_name="SGForCrossAccessAPIGW"
+                                           )
+
+        security_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22))
+        security_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(443))
+
+        # VPC endpoint to access private API Gateway in antoher account.
+        vpc.add_interface_endpoint(
+            "VPCEForCrossAccessAPIGW",
+            service=ec2.InterfaceVpcEndpointService(
+                name="com.amazonaws.us-west-2.execute-api"),
+            security_groups=[security_group],
+            private_dns_enabled=True
         )
